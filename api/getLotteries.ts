c8bc9382710770528b22042a7a00b7e7864013a5
秋 奈月 <43605695+akinazuki@users.noreply.github.com>
2024-02-03 11:59:10 +0900

    unique by qqNumber

--- api/getLotteries.ts
+++ api/getLotteries.ts
@@ -9,6 +9,11 @@ export default async function handler(
   if (list.length === 0 || !list)
     return response.status(200).json([])
 
-  const result = await kv.mget(list)
+  const result = await kv.mget(list).then(values => uniqueArrayByKey(values, 'qqNumber'))
+
   return response.status(200).json(result)
+}
+
+function uniqueArrayByKey(array: any[], key: string) {
+  return [...new Map(array.map(item => [item[key], item])).values()]
 }
