d56f8c357e6c686b4631ef28f4ae867a5533a593
秋 奈月 <43605695+akinazuki@users.noreply.github.com>
2025-11-26 00:30:15 +0900

    Refactor Roll.vue and update dependencies
    Reworked Roll.vue to use local data, improved random number generation, adjusted UI elements, and updated logic for rolling and displaying results. Updated multiple dependencies in package.json to latest versions for improved compatibility and features.

--- src/components/Roll.vue
+++ src/components/Roll.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
 import { computedAsync } from '@vueuse/core'
-
 import _ from 'lodash'
 import {
   computed,
   onMounted,
   ref,
 } from 'vue'
-
 import wait from 'wait'
+
+import qqData from './data.json'
 
 const listUrl = '/api/getLotteries'
 const currentIndex = ref(0)
@@ -19,10 +19,10 @@ const targetNickname = ref('')
 const targetQqNumber = ref('')
 const cardScale = ref(1)
 const showAward = ref(false)
-const showLine = ref(true)
+const showLine = ref(false)
 const blurFilter = ref(null as SVGElement | null)
 const clicked = ref(false)
-const defaultAnimationTime = 10
+const defaultAnimationTime = 1
 const cls = ref(0)
 const clsNames = [
   '幻',
@@ -49,12 +49,21 @@ const audio = computed(() =>
 )
 
 const list = computedAsync(async () => {
-  const res = await fetch(listUrl)
+  // const res = await fetch(listUrl)
+  // const data: {
+  //   qqNumber: string
+  //   nickname: string
+  //   time: number
+  // }[] = await res.json()
   const data: {
     qqNumber: string
     nickname: string
     time: number
-  }[] = await res.json()
+  }[] = qqData.map(item => ({
+    qqNumber: item.uin.toString(),
+    nickname: item.nick,
+    time: Date.now(),
+  }))
   return data
 })
 
@@ -96,18 +105,45 @@ async function stopRoll(target: number) {
   await wait(10)
   time.value = defaultAnimationTime
   currentIndex.value = target
+  console.log('stopping at', target)
   return wait(defaultAnimationTime * 1000)
 }
 
+const fetchingRandom = ref(false)
+async function randomGen(): Promise<number> {
+  fetchingRandom.value = true
+  const api = `https://www.random.org/integers/?num=1&min=4950&max=9959&col=1&base=10&format=plain&rnd=new`
+  const randomNumber = await fetch(api).then(r => r.text()).then(t => t.trim()).catch(() => {
+    console.warn('random.org 获取随机数失败，使用 Math.random() 作为后备')
+    fetchingRandom.value = false
+    return String(Math.floor(Math.random() * 100000) + 1)
+  })
+  const rndStr = `${randomNumber.substring(0, 3)}.${randomNumber.substring(3)}`
+  const rnd = Number.parseFloat(rndStr)
+  console.log(`rndStr: ${rndStr}`)
+  return rnd
+}
 async function stop() {
-  let target = 0
-  do
-    target = (Math.random() * 500) + 495
-    // console.log(currentIndex.value)
-  while ((target % 1 <= 0.55) && (target % 1 >= 0.45))
+  const target = await randomGen()
+  // const rnd = Math.random()
+  // do {
+  //   const rnd = await randomGen()
+  //   target = (rnd * 500) + 495
+  // }
+  // while ((target % 1 <= 0.55) && (target % 1 >= 0.45))
+  fetchingRandom.value = false
+  showLine.value = true
   await stopRoll(target)
   const lotteried = Math.round(target + 3)
   const data = document.querySelector(`[data-index="${lotteried}"]`) as HTMLElement
+  console.log({
+    lotteried,
+    data: {
+      nickname: data.dataset.nickname,
+      qqNumber: data.dataset.qqnumber,
+      index: data.dataset.index,
+    },
+  })
   diceComplete(data)
 }
 
@@ -186,7 +222,7 @@ function reloadPage() {
   targetQqNumber.value = ''
   cardScale.value = 1
   showAward.value = false
-  showLine.value = true
+  showLine.value = false
   clicked.value = false
 }
 
@@ -213,8 +249,8 @@ function loadAssets() {
   })
 }
 onMounted(() => {
-  if (!isFirefox())
-    alert('请使用火狐浏览器打开本页面，否则可能会出现严重的渲染问题')
+  // if (!isFirefox())
+  // alert('请使用火狐浏览器打开本页面，否则可能会出现严重的渲染问题')
 
   blurFilter.value = document.querySelector('#blur > feGaussianBlur') // the feGaussianBlur primitive
   console.log(blurFilter.value)
@@ -235,52 +271,108 @@ onMounted(() => {
     </div>
     <div class="bgog h-screen w-screen fixed">
       <div v-if="pray" class="h-screen w-screen flex items-end justify-end text-white z-50">
-        <div class="mx-4 my-2 flex items-start w-32 drop-shadow-md font-semibold text-xl">
+        <div class="mx-16 my-8 flex items-start w-48 drop-shadow-md font-semibold text-2xl">
           少女祈祷中{{ '.'.repeat(dotCount) }}
         </div>
       </div>
     </div>
-    <div v-if="loaded" class="w-[90vw] h-screen overflow-x-hidden">
-      <button class="fixed mt-5 top-0 right-0 m-2 z-50 bg-gray-500 shadow-md rounded-md text-gray-100 px-5 py-1" type="button" @click="toggleFullScreen">
+    <div v-if="loaded" class="w-full lg:mx-16 mx-4 h-screen overflow-x-hidden">
+      <button
+        class="fixed mt-5 top-0 right-4 lg:right-24 m-2 z-50 bg-gray-500 shadow-md rounded-md text-gray-100 px-5 py-1"
+        type="button" @click="toggleFullScreen"
+      >
         全屏
       </button>
-      <div v-if="!clicked" class="fixed flex m-2 z-50 w-[90vw] mt-5">
-        <button class="bg-purple-500 shadow-md rounded-md text-purple-100 px-5 py-1 mx-3" type="button" @click="startRoll(0)">
+      <div v-if="!clicked" class="fixed flex m-2 z-50 mt-5">
+        <button
+          class="bg-purple-500 shadow-md rounded-md text-purple-100 px-5 py-1 mx-3" type="button"
+          @click="startRoll(0)"
+        >
           幻
         </button>
         <button class="bg-red-500 shadow-md rounded-md text-red-100 px-5 py-1 mx-3" type="button" @click="startRoll(1)">
           夢
         </button>
-        <button class="bg-blue-500 shadow-md rounded-md text-blue-100 px-5 py-1 mx-3" type="button" @click="startRoll(2)">
+        <button
+          class="bg-blue-500 shadow-md rounded-md text-blue-100 px-5 py-1 mx-3" type="button"
+          @click="startRoll(2)"
+        >
           結
         </button>
       </div>
-      <div v-else class="fixed flex m-2 z-50 w-[90vw] mt-5">
-        <button class="bg-green-500 shadow-md rounded-md text-green-100 px-5 py-1 mx-3" type="button" @click="reloadPage">
+      <div v-else class="fixed flex m-2 z-50 mt-5">
+        <button
+          v-show="!fetchingRandom" class="bg-green-500 shadow-md rounded-md text-green-100 px-5 py-1 mx-3"
+          type="button" @click="reloadPage"
+        >
           重新开始
         </button>
+        <span v-if="fetchingRandom" class="text-white px-5 py-1 mx-3">
+          正在获取随机数...
+        </span>
       </div>
       <div class="fixed m-auto w-[90vw] h-48 left-0 right-0 bottom-0 top-0 flex items-center justify-center z-40">
         <!-- <div class="h-40 w-40 z-50 gl" /> -->
-        <div v-show="showLine" class="h-40 w-[3px] bg-yellow-300 m-auto shadow-yellow-600 shadow-md z-50" />
+        <!-- <div
+          v-show="showLine"
+          :class="{
+            // bg-yellow-300 m-auto shadow-yellow-600
+            'bg-purple-500 m-auto shadow-purple-700': showLine && cls === 0,
+            'bg-red-500 m-auto shadow-red-700': showLine && cls === 1,
+            'bg-blue-500 m-auto shadow-blue-700': showLine && cls === 2,
+          }"
+          class="h-40 w-[3px]  shadow-md z-50"
+        /> -->
+        <div v-show="showLine" class="h-48 w-[3px] bg-yellow-300 m-auto shadow-yellow-600 shadow-md z-50" />
         <!-- <div class="h-40 w-40 z-50 gr" /> -->
       </div>
 
       <div class="h-full w-full">
-        <div :class="{ 'justify-center': !showList }" class="flex items-center shrink-0 h-full w-full" :style="`transition: all ${time}s ${easing};transform: translateX(-${currentIndex * 20}%)`">
+        <div
+          :class="{ 'justify-center': !showList }" class="flex gap-2 items-center shrink-0 h-full w-full"
+          :style="`transition: all ${time}s ${easing};transform: translateX(-${currentIndex * 20}%)`"
+        >
           <template v-if="showList">
-            <div v-for="(item) in 1000" :key="item" class="svg-motion-blur ibg mx-1 flex flex-col h-40 rounded-lg items-center justify-center w-[calc(20%-0.5rem)] shrink-0 shadow-md overflow-hidden">
-              <img :src="`https://q1.qlogo.cn/g?b=qq&nk=${tripleList[item % tripleList.length]?.qqNumber}&s=640`" class="mt-auto rounded-full size-24 drop-shadow-md">
-              <span class="text-white drop-shadow-md mt-1 mb-auto text-lg font-semibold max-w-32 truncate" :data-index="item" :data-nickname="tripleList[item % tripleList.length]?.nickname" :data-qqNumber="tripleList[item % tripleList.length]?.qqNumber">{{ tripleList[item % tripleList.length].nickname }}</span>
+            <div
+              v-for="(item) in 1000" :key="item"
+              class="svg-motion-blur ibg flex flex-col gap-3 h-48 rounded-lg items-center justify-between w-[calc(20%-0.5rem)] shrink-0 shadow-md overflow-hidden"
+            >
+              <img
+                :src="`https://q1.qlogo.cn/g?b=qq&nk=${tripleList[item % tripleList.length]?.qqNumber}&s=640`"
+                class="mt-auto rounded-full size-24 drop-shadow-md"
+              >
+              <span
+                class="text-white drop-shadow-md mt-1 mb-auto text-lg font-semibold max-w-64 truncate"
+                :data-index="item" :data-nickname="tripleList[item % tripleList.length]?.nickname"
+                :data-qqNumber="tripleList[item % tripleList.length]?.qqNumber"
+                v-html="tripleList[item % tripleList.length].nickname"
+              />
             </div>
           </template>
-          <div v-else :style="`transform: scale(${cardScale})`" class="scale m-auto z-50 ibg mx-1 flex h-40 rounded-lg items-center justify-center w-[calc(20%-0.5rem)] shrink-0 shadow-md overflow-hidden">
-            <div :class="{ 'flex-col': !showAward, 'gap-6': showAward }" class="scale flex justify-center items-center">
-              <img :src="`https://q1.qlogo.cn/g?b=qq&nk=${targetQqNumber}&s=640`" class="scale mt-auto rounded-full size-24 drop-shadow-md">
-              <div class="flex flex-col">
-                <span class="scale text-white drop-shadow-md mt-1 mb-auto text-lg font-semibold truncate max-w-32 ">{{ targetNickname }}</span>
-                <span v-if="showAward" class="scale text-white drop-shadow-md mt-1 mb-auto font-light">{{ targetQqNumber }}</span>
-                <div v-if="showAward" :class="[`bg-${awardColor[targetAward]}-500`, `text-${awardColor[targetAward]}-100`]" class="flex items-center justify-center size-8 mr-auto rounded-full scale drop-shadow-md mt-1 mb-auto text-lg font-semibold">
+          <div
+            v-else :style="`transform: scale(${cardScale})`"
+            class="scale z-50 ibg mx-1 flex h-fit rounded-lg items-center justify-center w-fit shrink-0 shadow-md overflow-hidden"
+          >
+            <div :class="{ 'flex-col items-center': !showAward, 'gap-6': showAward }" class="flex w-full p-8">
+              <img
+                :src="`https://q1.qlogo.cn/g?b=qq&nk=${targetQqNumber}&s=640`"
+                class="mt-auto rounded size-24 drop-shadow-md"
+              >
+              <div class="flex flex-col justify-between mb-1">
+                <div class="flex flex-col">
+                  <span
+                    class="text-white drop-shadow-md text-xl font-semibold truncate max-w-64"
+                    :class="{
+                      'min-w-32': showAward,
+                    }"
+                    v-html="targetNickname"
+                  />
+                  <span v-if="showAward" class="scale text-white drop-shadow-md font-light">{{ targetQqNumber }}</span>
+                </div>
+                <div
+                  v-if="showAward" :class="[`bg-${awardColor[targetAward]}-500`]"
+                  class="flex items-center justify-center size-7 mr-auto rounded scale drop-shadow-md text-lg font-semibold text-white"
+                >
                   <span>{{ targetAward }}</span>
                 </div>
               </div>
